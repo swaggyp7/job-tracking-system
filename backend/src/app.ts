16615968c29337e3d7bb74cb6c020e49edfc5b34
Yuteng Lin <76236579+swaggyp7@users.noreply.github.com>
2026-01-21 16:05:15 -0600

    feat: add CORS support to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 import apiRouter from "./routes";
 import { initDatabase } from "./db/database";
 
 const app = express();
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api", apiRouter);
